49c9374928caa79bf29b162f10a62cbd3df8f333
Wiktor Maciejewski <104524608+Wektors@users.noreply.github.com>
2022-08-15 18:57:58 +0200

    Update roman-numeral-converter.js

--- roman-numeral-converter.js
+++ roman-numeral-converter.js
@@ -29,10 +29,10 @@ function convD0(num) {
     num = 'VI'
   }
   if (num == 7) {
-    num = 'VI'
+    num = 'VII'
   }
   if (num == 8) {
-    num = 'VII'
+    num = 'VIII'
   }
   if (num == 9) {
     num = 'IX'
@@ -77,6 +77,52 @@ function convD1(num) {
   return num
 }
 
+function convD2(num) {
+  if (num == 1) {
+    num = 'C'
+  }
+  if (num == 2) {
+    num = 'CC'
+  }
+  if (num == 3) {
+    num = 'CCC'
+  }
+  if (num == 4) {
+    num = 'CD'
+  }
+  if (num == 5) {
+    num = 'D'
+  }
+  if (num == 6) {
+    num = 'DC'
+  }
+  if (num == 7) {
+    num = 'DCC'
+  }
+  if (num == 8) {
+    num = 'DCCC'
+  }
+  if (num == 9) {
+    num = 'CM'
+  }
+  if (num == 0) {
+    num = ''
+  }
+  return num
+}
+
+function convD3(num) {
+  if (num == 1) {
+    num = 'M'
+  }
+  if (num == 2) {
+    num = 'MM'
+  }
+  if (num == 3) {
+    num = 'MMM'
+  }
+  return num
+}
 
 
 if (numArr.length == 1) {
@@ -88,10 +134,22 @@ if (numArr.length == 2) {
   romArr.unshift(convD1(numArr[numArr.length - 2]));
 }
 
+if (numArr.length == 3) {
+  romArr.unshift(convD0(numArr[numArr.length - 1]));
+  romArr.unshift(convD1(numArr[numArr.length - 2]));
+  romArr.unshift(convD2(numArr[numArr.length - 3]));
+}
+
+if (numArr.length == 4) {
+  romArr.unshift(convD0(numArr[numArr.length - 1]));
+  romArr.unshift(convD1(numArr[numArr.length - 2]));
+  romArr.unshift(convD2(numArr[numArr.length - 3]));
+  romArr.unshift(convD3(numArr[numArr.length - 4]));
+}
+
 let romStr = romArr.join('')
 
 return romStr;
 }
 
-
-console.log(convertToRoman(36));
+console.log(convertToRoman(1000))

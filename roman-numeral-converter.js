98db8a369c8275eab280357d9584af1f62714b8f
Wiktor Maciejewski <104524608+Wektors@users.noreply.github.com>
2022-08-15 19:10:16 +0200

    Update roman-numeral-converter.js
    shorter version

--- roman-numeral-converter.js
+++ roman-numeral-converter.js
@@ -6,145 +6,64 @@ let numArr = numStr.split("");
 
 let romArr = []
 
-// function names come from positions in decimal
-// last digit 'D0' because 10^0 = 1;
 
-function convD0(num) {
+function convDigit(num, l1, l2, l3) {
   if (num == 1) {
-    num = 'I'
+    num = l1
   }
   if (num == 2) {
-    num = 'II'
+    num = l1 + l1
   }
   if (num == 3) {
-    num = 'III'
+    num = l1 + l1 + l1
   }
   if (num == 4) {
-    num = 'IV'
+    num = l1 + l2
   }
   if (num == 5) {
-    num = 'V'
+    num = l2
   }
   if (num == 6) {
-    num = 'VI'
+    num = l2 + l1
   }
   if (num == 7) {
-    num = 'VII'
+    num = l2 + l1 + l1
   }
   if (num == 8) {
-    num = 'VIII'
+    num = l2 + l1 + l1 + l1
   }
   if (num == 9) {
-    num = 'IX'
+    num = l1 + l3
   }
   if (num == 0) {
     num = ''
-  }
-  return num
-}
-
-function convD1(num) {
-  if (num == 1) {
-    num = 'X'
-  }
-  if (num == 2) {
-    num = 'XX'
-  }
-  if (num == 3) {
-    num = 'XXX'
-  }
-  if (num == 4) {
-    num = 'XL'
-  }
-  if (num == 5) {
-    num = 'L'
-  }
-  if (num == 6) {
-    num = 'LX'
-  }
-  if (num == 7) {
-    num = 'LXX'
-  }
-  if (num == 8) {
-    num = 'LXXX'
-  }
-  if (num == 9) {
-    num = 'XC'
-  }
-  if (num == 0) {
-    num = ''
-  }
-  return num
-}
-
-function convD2(num) {
-  if (num == 1) {
-    num = 'C'
-  }
-  if (num == 2) {
-    num = 'CC'
-  }
-  if (num == 3) {
-    num = 'CCC'
-  }
-  if (num == 4) {
-    num = 'CD'
-  }
-  if (num == 5) {
-    num = 'D'
-  }
-  if (num == 6) {
-    num = 'DC'
-  }
-  if (num == 7) {
-    num = 'DCC'
-  }
-  if (num == 8) {
-    num = 'DCCC'
-  }
-  if (num == 9) {
-    num = 'CM'
-  }
-  if (num == 0) {
-    num = ''
-  }
-  return num
-}
-
-function convD3(num) {
-  if (num == 1) {
-    num = 'M'
-  }
-  if (num == 2) {
-    num = 'MM'
-  }
-  if (num == 3) {
-    num = 'MMM'
   }
   return num
 }
 
 
 if (numArr.length == 1) {
-  romArr.unshift(convD0(numArr[numArr.length - 1]));
+  romArr.unshift(convDigit(numArr[numArr.length - 1], 'I', 'V', 'X'));
 }
 
 if (numArr.length == 2) {
-  romArr.unshift(convD0(numArr[numArr.length - 1]));
-  romArr.unshift(convD1(numArr[numArr.length - 2]));
+  romArr.unshift(convDigit(numArr[numArr.length - 1], 'I', 'V', 'X'));
+  romArr.unshift(convDigit(numArr[numArr.length - 2], 'X', 'L', 'C'));
+  
+
 }
 
 if (numArr.length == 3) {
-  romArr.unshift(convD0(numArr[numArr.length - 1]));
-  romArr.unshift(convD1(numArr[numArr.length - 2]));
-  romArr.unshift(convD2(numArr[numArr.length - 3]));
+  romArr.unshift(convDigit(numArr[numArr.length - 1], 'I', 'V', 'X'));
+  romArr.unshift(convDigit(numArr[numArr.length - 2], 'X', 'L', 'C'));
+  romArr.unshift(convDigit(numArr[numArr.length - 3], 'C', 'D', 'M'));
 }
 
 if (numArr.length == 4) {
-  romArr.unshift(convD0(numArr[numArr.length - 1]));
-  romArr.unshift(convD1(numArr[numArr.length - 2]));
-  romArr.unshift(convD2(numArr[numArr.length - 3]));
-  romArr.unshift(convD3(numArr[numArr.length - 4]));
+  romArr.unshift(convDigit(numArr[numArr.length - 1], 'I', 'V', 'X'));
+  romArr.unshift(convDigit(numArr[numArr.length - 2], 'X', 'L', 'C'));
+  romArr.unshift(convDigit(numArr[numArr.length - 3], 'C', 'D', 'M'));
+  romArr.unshift(convDigit(numArr[numArr.length - 4], 'M', ' ', ' '));
 }
 
 let romStr = romArr.join('')
@@ -152,4 +71,4 @@ let romStr = romArr.join('')
 return romStr;
 }
 
-console.log(convertToRoman(1000))
+console.log(convertToRoman(4))
